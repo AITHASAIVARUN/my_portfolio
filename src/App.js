66f90140dd0071072ae4c19eb48328d236eecbb0
Saivarun Aitha <saivarun.aitha18103@gmail.com>
2025-01-08 00:36:59 +0530

    Herosection completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import Home from "./Pages/Home/Homescreen";
+import Navbar from "./Pages/Home/Navbar";
 import React from "react";
 import { BrowserRouter as Router , Route ,Routes} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <Router>
+        <div>
+          <Navbar></Navbar>
+          <Routes>
+            <Route path='/' element={<Home></Home>}></Route>
+            <Route path='*' element={<div>Dude yo came to the wrong Address</div>}></Route>
+            <Route path='/' element={<Home></Home>}></Route>
+          </Routes>
+        </div>
+
+      </Router>
       <Home></Home>
     </div>
   );
--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,7 +1,28 @@
-export default function HeroSection(){
-   return(
-   <section id="herosection" className="hero--section">
-
-   </section>
-   );
+export default function HeroSection() {
+  return (
+    <section id="herosection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey I'm Sai Varun Aitha</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section--title--color">FULLSTACK</span>{" "}
+            <br />
+            DEVELOPER
+          </h1>
+          <p className="hero--section--description">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            <br />
+            Repudiandae sunt nihil reiciendis voluptatibus nemo dolorem dolores
+            veritatis rem perferendis, quas magnam sapiente voluptatem. Dicta
+            accusamus, cupiditate quidem repellendus voluptates iste?
+          </p>
+        </div>
+        <button className="btn  btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.jpg" alt="Hero-img" />
+      </div>
+    </section>
+  );
 }
+ 
