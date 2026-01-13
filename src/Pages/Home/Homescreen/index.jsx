8e21882e6b7aad7aa0aeeec15d485a44e720daf0
Saivarun Aitha <108663808+AITHASAIVARUN@users.noreply.github.com>
2026-01-13 21:40:23 +0530

    Update index.jsx

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -2,8 +2,6 @@ import AboutMe from "../AboutMe";
 import HeroSection from "../HeroSection";
 import Myskills from "../MySkills";
 import MyPortfolio from "../MyPortfolio";
-import Testimonial from "../Testimonials";
-import Contact from "../Contact";
 import Footer from "../Footer";
 
 export default function Home() {
@@ -13,7 +11,6 @@ export default function Home() {
       <Myskills></Myskills>
       <AboutMe/>
       <MyPortfolio/>
-      <Testimonial/>
       <Contact/>
       <Footer/>
     </>
